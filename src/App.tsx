a099e35100bc33a4ec22d7d3d03435e477df93ed
Tyler Chen <chentyler52@gmail.com>
2022-11-16 17:33:15 -0700

    Created AnswerObject and set the type for our array types in our states

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { useState } from "react";
+import { fetchQuizQuestions, Question } from "./API";
 
 // Components
 import QuestionCard from "./components/QuestionCard";
+
+// Types
+import { QuestionState, Difficulty } from "./API";
+
+type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10; //set from API
 
 const App = () => {
     // Set states
     const [loading, setLoading] = useState(false); // set default to false
-    const [questions, setQuestions] = useState([]);
+    const [questions, setQuestions] = useState<QuestionState[]>([]);
     const [number, setNumber] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([]);
+    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
     const [score, setScore] = useState(0);
     const [gameOver, setGameOver] = useState(true);
 
-    const startTrivia = async () => {};
+    console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+    const startTrivia = async () => {
+		
+	};
 
     const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
@@ -29,14 +44,14 @@ const App = () => {
             </button>
             <p className="score">Score: </p>
             <p>Loading Questions ...</p>
-            <QuestionCard 
+            {/* <QuestionCard 
 				questionNum={number + 1}
 				totalQuestions={TOTAL_QUESTIONS}
 				question={questions[number]}
 				answers={questions[number].answer}
 				userAnswer={userAnswers ? userAnswers[number]: undefined}
 				callback={checkAnswer}
-			/>
+			/> */}
             <button className="next" onClick={nextQuestion}>
                 Next Question
             </button>
